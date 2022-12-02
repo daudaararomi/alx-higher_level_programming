8f3fdc21568930b96ea63955e2f512a1b1a53246
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-12-01 19:38:07 -0800

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
         print("1 argument:")
     else:
         print("{} arguments:".format(count))
-    for i in range(1, count):
-        print("{}: {}".format(i, sys.argv[i]))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
