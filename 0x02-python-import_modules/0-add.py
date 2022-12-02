f0cf47b018257188e3069c44c9bb807143eda930
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-12-01 18:54:48 -0800

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-# a program that imports the function def add(a, b): from the file add_0.py and
-# pts the result of the addition 1 + 2 = 3
-from add_0 import add
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+# 0_add.py
+# Print the sum of 1 and 2.
+
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
