f719b87f871a0edc827bdf26ad6fb0f2a7fce45e
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-12-03 13:30:39 -0800

    9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# max_integer
+#  finds the biggest integer of a list.
+
+
+def max_integer(my_list=[]):
+    max = 0
+    if len(my_list) == 0:
+        return (None)
+    for i in range(len(my_list)):
+        if my_list[i] > max:
+            max = my_list[i]
+
+    return (max)
