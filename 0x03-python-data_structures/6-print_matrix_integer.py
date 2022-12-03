c82c16a40c41fe77eb0d304b5558dc430d34683b
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-12-03 08:54:04 -0800

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,13 @@
-#!/usr/bin/phyton3
-# print_matrix_integer:prints a matrix of integers.
+#!/usr/bin/python3
+# 6-print_matrix_integer.py
+# Print a matrix of integers
+
 
 def print_matrix_integer(matrix=[[]]):
-
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end=" ")
+            print("{:d}".format(matrix[i][j]), end="")
             if j != (len(matrix[i]) - 1):
                 print(" ", end="")
+
         print("")
