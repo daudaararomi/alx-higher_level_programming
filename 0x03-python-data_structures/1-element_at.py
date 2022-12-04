7e947e5313ef7d2b9d5ce2e9b93a585ea2e90113
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-12-03 20:08:45 -0800

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,6 @@
 # 1-element_at.py:Retrive an element from a list.
 
 def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list) -1
-    return None
-return my_lst[idx]
+    if idx < 0 or idx > len(my_list):
+        return None
+    return (my_lst[idx])
