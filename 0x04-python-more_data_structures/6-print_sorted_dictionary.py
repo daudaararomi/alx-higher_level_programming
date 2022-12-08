ea0bcdffb3e8b30ee7c0d354ab5773d237889fdb
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-12-07 19:52:48 -0800

    6-print_sorted_dictionary.py

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# print_sorted_dictionary:
+# prints a dictionary by ordered keys
+def print_sorted_dictionary(a_dictionary):
+    [print("{}: {}".format(k, a_dictionary[k])) for k in sorted(a_dictionary)]
