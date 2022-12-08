9f8e98609262be9b1d31bc72d12eb3472ccad33c
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-12-07 20:30:17 -0800

    8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -2,6 +2,6 @@
 # simple_delete
 # deletes a key in a dictionary
 def simple_delete(a_dictionary, key=""):
-    if key  in a_dictionary:
+    if key in a_dictionary:
         del a_dictionary[key]
     return (a_dictionary)
