c9c86ddf8237b6db6a6bea68c6dc1b925080b8c5
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-12-07 20:15:46 -0800

    7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,10 +2,5 @@
 # update_dictionary:
 # replaces or adds key/value in a dictionary
 def update_dictionary(a_dictionary, key, value):
-    for k in a_dictionary:
-        if k == key:
-            del a_dictionary[k]
-            a_dictionary[key] = value
-        else:
-            a_dictionary[key] = value
-    return (a_dictionary)
+    a_dictionary[key] = value
+        return (a_dictionary)
