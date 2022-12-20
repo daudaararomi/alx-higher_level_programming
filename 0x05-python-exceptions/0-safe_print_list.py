386980e21b3987a1755f465f1737a2e0a3c23679
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-12-19 17:39:43 -0800

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end='')
             i = i + 1
-        except IndeError:
+        except IndexError:
             break
     print()
     return i
