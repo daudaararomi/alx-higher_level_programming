742c7253c8798b667b467b647fe66f37c1744341
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-12-19 18:07:07 -0800

    1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,8 +3,7 @@
 # arg : value - can be any type (integer, string, etc.)
 def safe_print_integer(value):
     try:
-        if value is int:
-            print("{:d}".format(value))
-            return (True)
+        print("{:d}".format(value))
+        return (True)
     except (TypeError, ValueError):
         return (False)
