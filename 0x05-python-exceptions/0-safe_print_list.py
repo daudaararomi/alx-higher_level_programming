65441b02c18c401966faa8869fdae5643f40d9cb
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-12-19 17:36:46 -0800

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,14 +2,12 @@
 #safe_print_list - prints x elements of a list
 
 def safe_print_list(my_list=[], x=0):
-    try:
-        i = 0
-        while i < x:
+    i = 0
+    while i < x:
+        try:
             print("{}".format(my_list[i]), end='')
             i = i + 1
-        print()
-        return i
-    except Index error:
-        break
+        except IndeError:
+            break
     print()
     return i
