21388895715c463f6e8a4614e7fd2cf0200cce66
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2023-01-10 13:53:33 -0800

    2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,21 +1,12 @@
 #!/usr/bin/python3
-"""read_lines
+"""append_write
 """
-import os
 
 
-def read_lines(filename="", nb_lines=0):
-    """Takes in str filename to read, and n lines w/ int nb_lines
+def append_write(filename="", text=""):
+    """Takes in str filename to write to, and str text to append to file
     """
 
-
-    with open(filename, encoding="utf-8") as readFile:
-        lineNum = 0
-        while True:
-            line = readFile.readline()
-            lineNum += 1
-            print(line, end='')
-            if lineNum >= nb_lines and nb_lines > 0:
-                break
-            if not line:
-                break
+    with open(filename, mode="a", encoding="utf-8") as appendFile:
+        appendFile.write(text)
+        return len(text)
