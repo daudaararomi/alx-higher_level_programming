62dcc57cd5e775f1f907c5d7dbf240aa6f576141
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2023-01-10 20:31:19 -0800

    9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -11,6 +11,6 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-        def to_json(self):
-            """ retrieves dictionary representation of a Student instance """
-            return self.__dict__
+    def to_json(self):
+        """ retrieves dictionary representation of a Student instance """
+        return self.__dict__
