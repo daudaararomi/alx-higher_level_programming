18fc0696ea09189b0e03ca391efb2da688ea3227
root <daudaararomi@gmail.com>
2023-01-19 14:41:45 -0800

    square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -25,3 +25,15 @@ class Square(Rectangle):
         """ prints the unoffical string representation of square """
         return "[Square] ({:d}) {:d}/{:d} - {:d}"\
             .format(self.id, self.x, self.y, self.width)
+
+    def update(self, *args, **kwargs):
+        """ assigns attributes to square """
+        if len(args) != 0:
+            i = 0
+            square_attrs = ["id", "size", "x", "y"]
+            for arg in args:
+                setattr(self, square_attrs[i], args[i])
+                i += 1
+        else:
+            for k, v in kwargs.items():
+                setattr(self, k, v)
