21c001b82e94c99ef8bc3d49a54982085f8a16e7
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2023-02-16 05:40:50 -0800

    4-first_table.sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,5 @@
--- Create a table in the current databse
---Query to create a table called first_table in the current database
+-- Creates a table in the current database
+-- Query to create a table called first_table in the current database in your MySQL server
 CREATE TABLE IF NOT EXISTS first_table (
-	id INT,
-	name VARCHAR(256));
-
+id INT,
+name VARCHAR(256));
